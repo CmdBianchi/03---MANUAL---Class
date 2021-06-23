a0ef718b00c31abc72a1abe2ac3c238b5f821f3b
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-23 14:24:52 -0300

    Adicionar arquivos de projeto.

---
+++ 03 - MANUAL - Class/Entities/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Entities {
+    class Calculator {
+        public double X { get; set; }
+        public double Y  { get; set; }
+        public double Z { get; set; }
+
+        public Calculator() {
+        }      
+        public Calculator(double x, double y, double z) {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public double HeronP() {
+            return (X + Y + Z) * 0.5;
+        }
+        public double Area() {
+            return Math.Sqrt(HeronP() * (HeronP() - X) * (HeronP() - Y) * (HeronP() - Z));
+        }
+
+        public override string ToString() {
+            return "Valor de P: " + HeronP() + "\nValor da AREA: " + Area().ToString("F2");
+        }
+    }
+}
---
+++ 03 - MANUAL - Class/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Entities;
+namespace _03___MANUAL___Class {
+    class Program {
+        static void Main(string[] args) {
+
+            Calculator calculatorA = new Calculator();
+            Console.Write("Digite o valor do LADO A: "); calculatorA.X = double.Parse(Console.ReadLine());
+            Console.Write("Digite o valor do LADO B: "); calculatorA.Y = double.Parse(Console.ReadLine());
+            Console.Write("Digite o valor do LADO C: "); calculatorA.Z = double.Parse(Console.ReadLine());
+            Console.WriteLine("------------------");
+            Console.WriteLine(calculatorA);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Calculator calculatorB = new Calculator();
+            Console.Write("Digite o valor do LADO A: "); calculatorB.X = double.Parse(Console.ReadLine());
+            Console.Write("Digite o valor do LADO B: "); calculatorB.Y = double.Parse(Console.ReadLine());
+            Console.Write("Digite o valor do LADO C: "); calculatorB.Z = double.Parse(Console.ReadLine());
+            Console.WriteLine("------------------");
+            Console.WriteLine(calculatorB);
+            Console.WriteLine("");
+            Console.WriteLine("------------------");
+
+            if (calculatorA.Area() > calculatorB.Area()) {
+                Console.WriteLine("O triangulo 1 tem MAIOR AREA.");
+            }
+            else {
+                Console.WriteLine("O triangulo 2 tem MAIOR AREA.");
+            }
+
+            
+
+        }
+    }
+}
